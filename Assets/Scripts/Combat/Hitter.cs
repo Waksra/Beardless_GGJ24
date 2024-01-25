08f3ae8f23bb7a4e3dd6b190a367450ddeeb4fc7
Wiktor Ravndal <w.ravndal@gmail.com>
2024-01-25 23:25:22 +0100

    Improved the kick

--- Assets/Scripts/Combat/Hitter.cs
+++ Assets/Scripts/Combat/Hitter.cs
@@ -1,22 +1,47 @@
+using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Combat
 {
+    enum HitType
+    {
+        HitDirection,
+        Custom,
+        Forward
+    }
+
     public class Hitter : MonoBehaviour
     {
         [SerializeField] private float hitForce = 10f;
-        
+        [SerializeField] private HitType hitType = HitType.HitDirection;
+
+        [SerializeField, EnableIf("hitType", HitType.Custom)]
+        private Vector3 customHitDirection = Vector3.zero;
+
+        [Button, EnableIf("hitType", HitType.Custom)]
+        private void NormalizeCustomHitDirection()
+        {
+            customHitDirection = customHitDirection.normalized;
+        }
+
         public float HitForce
         {
             get => hitForce;
             set => hitForce = value;
         }
-        
+
+        private void Start()
+        {
+            NormalizeCustomHitDirection();
+        }
+
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out Hittable hittable))
             {
-                Vector3 direction = (other.transform.position - transform.position).normalized;
+                Vector3 direction = GetDirection(other);
                 hittable.Hit(new HitData(direction * hitForce));
             }
         }
@@ -25,8 +50,39 @@ namespace Combat
         {
             if (other.collider.TryGetComponent(out Hittable hittable))
             {
-                Vector3 direction = (other.transform.position - transform.position).normalized;
+                Vector3 direction = GetDirection(other);
+
                 hittable.Hit(new HitData(direction * hitForce));
+            }
+        }
+
+        private Vector3 GetDirection(Collision other)
+        {
+            switch (hitType)
+            {
+                case HitType.HitDirection:
+                    return (other.transform.position - transform.position).normalized;
+                case HitType.Custom:
+                    return transform.localToWorldMatrix.MultiplyVector(customHitDirection);
+                case HitType.Forward:
+                    return transform.forward;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private Vector3 GetDirection(Collider other)
+        {
+            switch (hitType)
+            {
+                case HitType.HitDirection:
+                    return (other.transform.position - transform.position).normalized;
+                case HitType.Custom:
+                    return transform.localToWorldMatrix.MultiplyVector(customHitDirection);
+                case HitType.Forward:
+                    return transform.forward;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
